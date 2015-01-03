a965d055826caf821987f2ff62a7c0f085316a32
paul.houlston <paul.houlston@hotmail.co.uk>
2015-01-03 18:32:36 +0000

    Added secondary tests for simple conversion of one level roman numerals

--- RomanNumeralKata/MyClass.cs
+++ RomanNumeralKata/MyClass.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace RomanNumeralKata
 {
 	class RomanNumeral
 	{
+		private static readonly IDictionary<int, string> Digits = new Dictionary<int, string> {
+			{ 1,"I" },
+			{ 5,"V" },
+			{ 10,"X" },
+			{ 50,"L" },
+			{ 100,"C" },
+			{ 500,"D" },
+			{ 1000,"M" },
+		};
+
 		public object ConvertFrom (int valueToConvert)
 		{
-			return "I";
+			return Digits[valueToConvert];
 		}
 	}
 
@@ -15,10 +26,16 @@ namespace RomanNumeralKata
 	{
 		class when_I_supply_a_known_roman_digit
 		{
-			[Test]
-			public void then_I_get_the_expected_output(){
-				var output = new RomanNumeral().ConvertFrom(1);
-				Assert.AreEqual("I", output);
+			[TestCase(1, "I")]
+			[TestCase(5, "V")]
+			[TestCase(10, "X")]
+			[TestCase(50, "L")]
+			[TestCase(100, "C")]
+			[TestCase(500, "D")]
+			[TestCase(1000, "M")]
+			public void then_I_get_the_expected_output(int valueToConvert, string expectedOutput){
+				var output = new RomanNumeral().ConvertFrom(valueToConvert);
+				Assert.AreEqual(expectedOutput, output);
 			}
 		}
 	}
